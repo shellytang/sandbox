20419bf2bb46b49b37d448c4217014afb900ae85
Shelly Tang <shellytang@gmail.com>
2018-01-17 10:26:39 -0800

    updated order section, positioned toppings

--- css-images/pizza/main.js
+++ css-images/pizza/main.js
@@ -5,12 +5,48 @@ $(function() {
     helper: 'clone'
   });
 
-  $('.pizza-container').droppable({
+  $('.right-container').droppable({
     drop: function(e, ui) {
       let newClone = $(ui.helper).clone();
       $(ui.helper).remove();
-      $(newClone).draggable();
+      $(newClone).draggable({
+        revert: true,
+      });
       $(this).after(newClone);
+      checkIfNewTopping(newClone);
     }
   });
+
+  const order = {};
+
+  //only update order if new topping
+  function checkIfNewTopping(topping) {
+    let toppingType = $(topping).attr('class').split(' ')[0];
+    if(!order[toppingType]) {
+      order[toppingType] = '$1.50';
+      updateOrder(toppingType);
+    }
+  }
+
+  function updateOrder(topping) {
+    if (topping === 'mushroom-box') {
+      topping = 'mushroom';
+    }
+    if (topping === 'olives-outer') {
+      topping = 'olives';
+    }
+    if (topping === 'bell-pepper') {
+      topping = 'bell pepper'; 
+    }
+    let toppingAddedToOrder = $('<p class="order-item">' + topping + '</p>');
+    $('.total').before(toppingAddedToOrder);
+    updateTotal();
+  }
+
+  function updateTotal() {
+    let count = Object.keys(order).length;
+    let total = ((count * 1.50)+16).toFixed(2);
+    $('.total').text('Total: $'+total);
+  }
+
 });
